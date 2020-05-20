ff2ed4433c81258995760be7ddd1f9cc595c2ccb
Murphy Sun <murphysunx@gmail.com>
2020-05-20 11:40:13 +1000

    初始化

---
+++ src/pages/product/Categories.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { PageHeaderWrapper } from '@ant-design/pro-layout';
+
+import CategoryTable from './CategoryTable';
+
+const TableList = () => {
+
+    return (
+        <PageHeaderWrapper>
+            <CategoryTable />
+        </PageHeaderWrapper>
+    );
+};
+
+export default TableList;
